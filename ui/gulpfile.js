48dc9b9728c1638f34ae4fd6e1e10c3bc64dfe75
Samuel's mpb18 <okosioc@gmail.com>
2020-04-27 02:43:21 +0800

    gulpfile.js update

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -6,7 +6,6 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     cleancss = require('gulp-clean-css'),// Minify css
     uglify = require('gulp-uglify'), // Minify js
-    imagemin = require('gulp-imagemin'), // Compresses PNG, JPEG, GIF and SVG images
     npmdist = require('gulp-npm-dist');
 
 // Paths
@@ -23,15 +22,11 @@ const paths = {
     },
     js: {
         dir: './static/assets/js'
-    },
-    img: {
-        dir: './static/assets/img',
-        files: './static/assets/img/**/*',
     }
 };
 
 // Styles autoprefixing and minification
-gulp.task('scss', function () {
+gulp.task('styles', function () {
     return gulp
         .src(paths.scss.main)
         .pipe(sass().on('error', sass.logError))
@@ -42,19 +37,15 @@ gulp.task('scss', function () {
         .pipe(gulp.dest(paths.css.dir));
 });
 
-// Image compression
-gulp.task('img', function () {
-    return gulp.src(paths.img.files)
-        .pipe(imagemin())
-        .pipe(gulp.dest(paths.img.dir))
-});
-
 // Scripts
-gulp.task('scripts', function () {
+gulp.task('scripts:copy', function () {
     // Copy libs from node_modules to vendor folder
-    gulp
+    return gulp
         .src(npmdist(), {base: paths.node})
         .pipe(gulp.dest(paths.vendor));
+
+});
+gulp.task('scripts:merge', function () {
     // Merge core libs into vendor.min.js, which will be shared in all pages
     gulp
         .src([
@@ -77,3 +68,9 @@ gulp.task('scripts', function () {
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(paths.js.dir));
 });
+
+// Default build command
+gulp.task('build', gulp.parallel(
+    'styles',
+    gulp.series('scripts:copy', 'scripts:merge')
+));
